88e149c35a689a1935f82a74fe789dacc241644e
NabokaEA <it_str@amaks-hotels.ru>
2020-12-10 21:46:35 +0200

    10 dec 2020_19:31 доработки по материалам вебинара 7

--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -3,17 +3,30 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ChatServer {
+
+    private boolean isRunning;
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public void setRunning(boolean running) {
+        isRunning = running;
+    }
+
+
     public ChatServer() {
+        isRunning = true;
         try (ServerSocket serverSocket = new ServerSocket(8189)) {
             System.out.println("Server started!");
-            while (true) {
+            while (isRunning) {
                 System.out.println("Server is waiting for connection");
-                Socket socket= serverSocket.accept();
-                System.out.println("Client accepted"+socket.getInetAddress().getHostName());
-               new Thread(new ClientHandler(socket)).start();
+                Socket socket = serverSocket.accept();
+                System.out.println("Client accepted" + socket.getInetAddress().getHostName());
+                new Thread(new ClientHandler(socket)).start();
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Server crushed");
         }
-}
+    }
 }
--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -1,34 +1,59 @@
 import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class ClientHandler implements Runnable, Closeable {
-    private Socket socket;
-    private DataInputStream inputStream;
-    private DataOutputStream outputStream;
+
+    private final DataInputStream inputStream;
+    private final DataOutputStream outputStream;
+    private static int connectionsCount = 0;
+    private int connectionID = 0;
+    private final String userName;
+    private boolean isRunning;
+    private final byte[] buffer;
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public void setRunning(boolean running) {
+        isRunning = running;
+    }
 
 
     public ClientHandler(Socket socket) throws IOException {
-        this.socket = socket;
+        connectionsCount++;
+        connectionID = connectionsCount;
+        userName = "user#" + connectionID;
         inputStream = new DataInputStream(socket.getInputStream());
         outputStream = new DataOutputStream(socket.getOutputStream());
+        isRunning = true;
+        buffer = new byte[256];
+
     }
 
     @Override
     public void run() {
-while (true){
-    try {
-        String messageFromClient = inputStream.readUTF();
-        if (messageFromClient.equals( "/end" )) {
-            System.out.println("Received \\End from client");
-            break ;
+        while (isRunning) {
+            try {
+                int bytesRead = inputStream.read(buffer);
+                String messageFromClient = new String(buffer, 0, bytesRead, StandardCharsets.UTF_8);
+                if (messageFromClient.equals("/end")) {
+                    System.out.println("Received \\End from client" + connectionID);
+                    break;
+                }
+                System.out.println("Received from client:" + userName + " " + messageFromClient);
+
+            } catch (IOException e) {
+                System.err.println("Exception while read");
+                break;
+            }
         }
-        System.out.println("Received from client: "+messageFromClient);
-        outputStream.writeUTF("Echo from server: "+messageFromClient);
-        outputStream.flush();
-    } catch (IOException e) {
-        e.printStackTrace();
     }
-}
+
+    public void sendMessage(String message) throws IOException {
+        outputStream.write((userName + ": " + message).getBytes(StandardCharsets.UTF_8));
+        outputStream.flush();
     }
 
     @Override
