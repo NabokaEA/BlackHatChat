6bdbbcffb019457d68cf5bd4df323d7df92e3013
NabokaEA <it_str@amaks-hotels.ru>
2020-12-24 19:48:16 +0200

    java3_lesson2 присоединена база данных, создание таблицы - все что успел до сдачи

---
+++ src/main/java/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        User user1=new User("Ivan", "UserDAO1","PassDAO1");
+
+    }
+}
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,5 +1,70 @@
-public class User implements UserDAO{
+import java.util.Objects;
+
+public class User implements UserDAO {
     private String userName;
+    private String userLastName;
     private String userNickName;
-    private  String userPass;
+    private String userPass;
+
+    public User(String userName, String userLastName, String userNickName, String userPass) {
+        this.userName = userName;
+        this.userLastName = userLastName;
+        this.userNickName = userNickName;
+        this.userPass = userPass;
+    }
+
+    public User(String userName, String userNickName, String userPass) {
+        this.userName = userName;
+        this.userNickName = userNickName;
+        this.userPass = userPass;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return getUserName().equals(user.getUserName())
+                && getUserNickName().equals(user.getUserNickName())
+                && getUserPass().equals(user.getUserPass());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUserName(),
+                getUserNickName(),
+                getUserPass());
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserNickName() {
+        return userNickName;
+    }
+
+    public void setUserNickName(String userNickName) {
+        this.userNickName = userNickName;
+    }
+
+    public String getUserPass() {
+        return userPass;
+    }
+
+    public void setUserPass(String userPass) {
+        this.userPass = userPass;
+    }
+
+    public String getUserLastName() {
+        return userLastName;
+    }
+
+    public void setUserLastName(String userLastName) {
+        this.userLastName = userLastName;
+    }
 }
